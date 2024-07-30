621d87deaf3f195cd8d02c873b16c66c6a8c8656
Arvindraj <arvindraj3172@gmail.com>
2024-07-30 21:06:26 +0530

    completed not found

--- app/server.go
+++ app/server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
@@ -18,11 +20,21 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+
 	//
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+
+	buf := make([]byte, 1024)
+	conn.Read(buf)
+
+	if !strings.HasPrefix(string(buf), "GET / HTTP/1.1") {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		conn.Close()
+		return
 	}
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
