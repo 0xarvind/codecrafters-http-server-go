be0aeb090ad6daa29308c815ae39cac24352dbe6
Arvindraj <arvindraj3172@gmail.com>
2024-07-31 00:11:59 +0530

    completed reading header

--- app/server.go
+++ app/server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net/http"
 	"strconv"
 	"strings"
 
@@ -29,15 +31,20 @@ func main() {
 		os.Exit(1)
 	}
 
-	buf := make([]byte, 1024)
-	conn.Read(buf)
-	fmt.Println("Request received: " + string(buf))
-	path := strings.Split(string(buf), " ")[1]
-	if strings.Contains(path, "/echo") {
+	request, err := http.ReadRequest(bufio.NewReader(conn))
+	if err != nil {
+		fmt.Println("Error reading request. ", err.Error())
+		return
+	}
+
+	path := request.URL.Path
+	if path == "/echo" {
 		body := strings.Split(path, "/")[2]
 		conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" + strconv.Itoa(len([]byte(body))) + "\r\n\r\n" + body))
-	} else if strings.HasPrefix(string(buf), "GET / HTTP/1.1") {
+	} else if path == "/" {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else if path == "/user-agent" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" + strconv.Itoa(len([]byte(request.UserAgent()))) + "\r\n\r\n" + request.UserAgent()))
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
